7d6c78a77d3aa7c340e9c91e6295764204b5dca9
Austen Williams <austen_will@yahoo.com>
2023-04-19 17:16:32 -0500

    final hw9 changes

--- students/models.py
+++ students/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
     
     def __str__(self) -> str:
     	return self.first_name + " " + self.last_name
